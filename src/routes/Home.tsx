961e96fa865a62d2cc07466162965eb885cdb082
RodericoNeto <rodericocrneto@gmail.com>
2023-07-06 23:45:59 -0300

    commit incial

---
+++ src/routes/Home.tsx
@@ -0,0 +1,55 @@
+import { UserProps } from "../types/user";
+
+import Search from "../components/Search";
+import User from "../components/User";
+import Error from "../components/Error";
+
+import { useState } from "react";
+
+const Home = () => {
+
+    //aqui o user pode ser de um dos dois tipos, inicia nulo mas depois vai ser do tipo que foi definido na pasta types
+    const [user, setUser] = useState<UserProps | null>(null);
+
+    const [error, setError] = useState(false);
+
+
+    const loadUser = async(userName: string) => {
+        setError(false);
+        setUser(null);
+
+        const res = await fetch(`https://api.github.com/users/${userName}`)
+
+        const data = await res.json()
+
+        if(res.status === 404){
+            setError(true)
+            return;
+        }
+
+        const {avatar_url, login, location, followers, following, html_url} = data
+
+        const userData: UserProps = {
+            avatar_url,
+            login,
+            location,
+            followers,
+            following,
+            html_url,
+        };
+
+        setUser(userData)
+    }
+
+    return( 
+    <div>
+        <Search loadUser={loadUser} />
+        
+        {user && <User {...user}/>}
+
+        {error && <Error />}
+    </div>
+    )
+}
+
+export default Home;
